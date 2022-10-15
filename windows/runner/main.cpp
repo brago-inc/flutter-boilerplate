f717ef6c6586fd5bb63f7762a85538db5ee76994
kimmy214 <upcwangying@126.com>
2022-10-15 20:39:58 +0800

    fix: product name

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,7 +27,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.CreateAndShow(L"flutter_template_ns", origin, size)) {
+  if (!window.CreateAndShow(L"flutter_games_boilerplate", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
